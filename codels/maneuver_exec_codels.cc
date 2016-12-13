47992c44760b5b2319dc54ad91d209e3a49761bd
Anthony Mallet <anthony.mallet@laas.fr>
2016-12-13 16:44:26 +0100

    Do not project current state on the trajectory to compute next desired state
    Just send the next desired state as is. This is what works experimentally
    better.

--- codels/maneuver_exec_codels.cc
+++ codels/maneuver_exec_codels.cc
@@ -92,13 +92,8 @@ mv_exec_main(const maneuver_state *state,
              const maneuver_desired *desired,
              const maneuver_log_s *log, genom_context self)
 {
-  const double dt = maneuver_control_period_ms * 1e-3;
-
   or_pose_estimator_state *sdata;
   struct timeval tv;
-  Eigen::Quaternion<double> qcur, qnext;
-  Eigen::Vector3d pcur, pnext;
-  double pdist, qdist;
 
   /* done? */
   if (trajectory->i >= trajectory->t._length) {
@@ -117,64 +112,9 @@ mv_exec_main(const maneuver_state *state,
       0.5 + sdata->ts.sec + 1e-9 * sdata->ts.nsec)
     return maneuver_pause_main;
 
-  pcur << sdata->pos._value.x, sdata->pos._value.y, sdata->pos._value.z;
-  if (sdata->vel._present) {
-    pcur += dt * Eigen::Vector3d(
-      sdata->vel._value.vx, sdata->vel._value.vy, sdata->vel._value.vz);
-  }
-  if (sdata->acc._present) {
-    pcur += 0.5 * dt * dt * Eigen::Vector3d(
-      sdata->acc._value.ax, sdata->acc._value.ay, sdata->acc._value.az);
-  }
-
-  qcur.w() = sdata->pos._value.qw;
-  qcur.vec() <<
-    sdata->pos._value.qx,
-    sdata->pos._value.qy,
-    sdata->pos._value.qz;
-
-  if (sdata->vel._present) {
-    Eigen::Vector3d w;
-    Eigen::Quaternion<double> wq;
-
-    w << sdata->vel._value.wx, sdata->vel._value.wy, sdata->vel._value.wz;
-
-    double a2 = dt * dt * w.squaredNorm();
-    if (a2 < 1e-1) {
-      wq.w() = 1 - a2/8 /*std::cos(a/2)*/;
-      wq.vec() = (0.5 - a2/48 /*std::sin(a/2)/a*/) * dt * w;
-    } else {
-      double a = std::sqrt(a2);
-      wq.w() = std::cos(a/2);
-      wq.vec() = std::sin(a/2)/a * dt * w;
-    }
-    qcur = wq * qcur;
-  }
-
-  /* get closest next sample */
-  for(pdist = qdist = std::numeric_limits<double>::max();
-      trajectory->i < trajectory->t._length; trajectory->i++) {
-    or_pose_estimator_state *n = &trajectory->t._buffer[trajectory->i];
-    double pd, qd;
-
-    pnext << n->pos._value.x, n->pos._value.y, n->pos._value.z;
-    qnext.w() = n->pos._value.qw;
-    qnext.vec() << n->pos._value.qx, n->pos._value.qy, n->pos._value.qz;
-
-    pd = (pnext-pcur).norm();
-    qd = qnext.angularDistance(qcur);
-    if (pd > pdist || qd > qdist) break;
-
-    pdist = pd;
-    qdist = qd;
-  }
-
   /* publish */
   sdata = desired->data(self);
-  if (trajectory->i >= trajectory->t._length)
-    *sdata = trajectory->t._buffer[trajectory->t._length - 1];
-  else
-    *sdata = trajectory->t._buffer[trajectory->i];
+  *sdata = trajectory->t._buffer[trajectory->i];
   sdata->ts.sec = tv.tv_sec;
   sdata->ts.nsec = 1000*tv.tv_usec;
   desired->write(self);
@@ -199,9 +139,7 @@ mv_exec_main(const maneuver_state *state,
   }
 
   /* next */
-  if (pdist > 0.1 || qdist > 0.2)
-    trajectory->i--;
-
+  trajectory->i++;
   return maneuver_pause_main;
 }
 
